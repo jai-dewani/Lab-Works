71a0c3c48f1b461b14bc284e0d63a4912d6e657a
jai-dewani <jai.dewani.99@gmail.com>
2019-05-12 15:02:53 +0530

    Add updated models

--- miniproject/models.py
+++ miniproject/models.py
@@ -6,16 +6,36 @@ from django.conf import settings
 class AccountUser(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,default=1,on_delete=models.CASCADE)
     phoneNumber = models.CharField(max_length=20)
-    accountType = models.CharField(max_length=30)
+    semester = models.CharField(max_length=10,default='1st')
+    # s -> Student
+    # P -> Prof
+
     def __str__(self):
         return self.user.username
+
+class Professor(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,default=1,on_delete=models.CASCADE)
+    phoneNumber = models.CharField(max_length=20)
+    def __str__(self):
+        return self.user.username
+
+class Subject(models.Model):
+    Sname = models.CharField(max_length=30)
+    Sprof = models.ForeignKey(Professor,on_delete=models.CASCADE)
+    Scredit = models.CharField(max_length=2)
+    Ssemester = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.Sname
 
 class Question(models.Model):
     QName = models.CharField(max_length=40)
     QCode = models.CharField(max_length=10)
     QDesc = models.TextField()
-    createdBy = models.ForeignKey(AccountUser,on_delete=models.CASCADE)
-    created = models.DateTimeField(default=datetime.now(), blank=True)
+    createdBy = models.ForeignKey(Professor,on_delete=models.CASCADE)
+    Qsubject = models.ForeignKey(Subject,default=1,on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(default=datetime.now, blank=True)
+
     def __str__(self):
         return self.QName
 
@@ -29,7 +49,7 @@ class Answers(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     code = models.TextField()
     submitedBy = models.ForeignKey(AccountUser,on_delete=models.CASCADE)
-    created = models.DateTimeField(default=datetime.now, blank=True)
+    submitedAt = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.question
