b99b17dab21d10bf34f090a55b50a490db43b6d3
jai-dewani <jai.dewani.99@gmail.com>
2019-05-12 18:15:03 +0530

    Update hackathon folder

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# urlpatterns 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('miniproject.urls'))
+    path('', include('miniproject.urls')),
 ]
+
